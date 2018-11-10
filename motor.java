8044c795d17d579d177447f409dba55e65040143
AirBisons14053 <44930191+AirBisons14053@users.noreply.github.com>
2018-11-10 12:25:11 -0800

    Update motor.java

--- motor.java
+++ motor.java
@@ -1,1 +1,24 @@
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+public class YourClassName extends OpMode {
+ 
+    DcMotor arm_motor;
+    public void init() {
+      arm_motor = hardwareMap.dcMotor.get("arm");
+      arm_motor.setDirection(DcMotor.Direction.REVERSE);
+    }
+    public void loop() {
+      arm_motor.setPower(-0.75);
+      int position = arm_motor.getCurrentPosition();
+      telemetry.addData("Encoder Position", position);
+      arm_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      arm_motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+      
+      float left_power = -gamepad1.left_stick_y;
+      float right_power = -gamepad1.right_stick_y;
+ 
+      left_drive.setPower(left_power);
+      right_drive.setPower(right_power);
+      
+    }
+}
