2cd2f16da1f025e70e8ad3c978b50d0e14a6e207
AirBisons14053 <44930191+AirBisons14053@users.noreply.github.com>
2018-11-10 13:13:23 -0800

    Update motor.java

--- motor.java
+++ motor.java
@@ -1,22 +1,15 @@
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-public class YourClassName extends OpMode {
+@Teleop(name = "Bison", group = "x")
+public class tryThisRED extends OpMode { //OpMode runs as long as the program is running, LinearOpMode only runs once
+    public DcMotor rightFront;
+    public DcMotor leftFront;
+    public DcMotor rightBack;
+    public DcMotor leftBack;
+    @Override public void runOpMode() {
+				
  
-    DcMotor arm_motor;
-    public void init() {
-      arm_motor = hardwareMap.dcMotor.get("arm");
-      arm_motor.setDirection(DcMotor.Direction.REVERSE);
-      arm_motor.setPower(-0.75);
-      int position = arm_motor.getCurrentPosition();
-      telemetry.addData("Encoder Position", position);
-      arm_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-      arm_motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    }
-    public void loop() {
-      float left_front_power = gamepad1.left_stick_y;
-      float right_front_power = -1 * gamepad1.left_stick_x;
-      float left_back_power = gamepad1.left_stick_y;
-      left_drive.setPower(left_power);
-      right_drive.setPower(right_power);
+
+ 
     }
 }
