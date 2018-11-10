216a4a5eaca023426b2418a39bbdf562849cc81f
AirBisons14053 <44930191+AirBisons14053@users.noreply.github.com>
2018-11-10 12:50:36 -0800

    Update motor.java

--- motor.java
+++ motor.java
@@ -6,19 +6,16 @@ public class YourClassName extends OpMode {
     public void init() {
       arm_motor = hardwareMap.dcMotor.get("arm");
       arm_motor.setDirection(DcMotor.Direction.REVERSE);
-    }
-    public void loop() {
       arm_motor.setPower(-0.75);
       int position = arm_motor.getCurrentPosition();
       telemetry.addData("Encoder Position", position);
       arm_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
       arm_motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-      
-      float left_power = -gamepad1.left_stick_y;
-      float right_power = -gamepad1.right_stick_y;
- 
+    }
+    public void loop() {
+      float left_power = gamepad1.left_stick_y;
+      float right_power = -1 * gamepad1.right_stick_y;
       left_drive.setPower(left_power);
       right_drive.setPower(right_power);
-      
     }
 }
